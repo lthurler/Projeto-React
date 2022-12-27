7f19c6cfde136adaf6af907e9a961d9007761656
lthurler <leothurler@gmail.com>
2022-12-27 18:55:31 -0300

    primeira parte

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Company</Link>
+          <Link to="/company">Contact</Link>
+          <Link to="/newproject">NewProject</Link>
+        </div>
+        <Container customClass="minHeight">
+        <Routes>          
+            <Route  path="/" element ={<Home/>} />
+            <Route  path="/company" element ={<Company />} />
+            <Route  path="/Contact" element ={<Contact />} />
+            <Route  path="/newproject" element ={<NewProject />} />          
+        </Routes>
+        </Container>
+        <p>Footer</p>
+      </Router>
+
   );
 }
 
